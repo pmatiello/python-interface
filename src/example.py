92ef95aa3269042394aacba88a4113f7687fc9e5
Pedro Matiello <pmatiello@gmail.com>
2011-05-10 22:58:08 -0300

    Support for kwargs.

--- src/example.py
+++ src/example.py
@@ -4,6 +4,7 @@ class comestible(interface):
     eat = method(["self"])
     buy_from = method(["self", "supermarket"])
     mix_with = method(["self"], varargs="ingredients")
+    cook_with = method(["self"], keywords="ingredients")
 
 
 @implements(comestible)
@@ -16,4 +17,7 @@ class hamburger(object):
         pass
     
     def mix_with(self, *ingredients):
+        pass
+    
+    def cook_with(self, **ingredients):
         pass
--- src/interface.py
+++ src/interface.py
@@ -4,9 +4,10 @@ class interface(object):
     pass
 
 class method(object):
-    def __init__(self, args=None, varargs=None):
+    def __init__(self, args=None, varargs=None, keywords=None):
         self.args = args or []
         self.varargs = varargs
+        self.keywords = keywords
 
 class implements(object):
     
@@ -22,6 +23,7 @@ class implements(object):
             impl_spec = getargspec(self._attribute(clazz, each))
             assert iface_spec.args == impl_spec.args
             assert iface_spec.varargs == impl_spec.varargs
+            assert iface_spec.keywords == impl_spec.keywords
             
 
     def _is_method(self, name):
