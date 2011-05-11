f05cfb91c9498024cad47523c66de964d4f041cf
Pedro Matiello <pmatiello@gmail.com>
2011-05-11 01:28:04 -0300

    Refactoring implements decorator.

--- src/interface.py
+++ src/interface.py
@@ -19,25 +19,42 @@ class implements(object):
         self.interface = interface
     
     def __call__(self, clazz):
-        target = dir(clazz)
         methods = [each for each in dir(self.interface) if self._is_method(each)]
         for each in methods:
-            assert each in target
-            iface_spec = self._attribute(self.interface, each)
-            impl_spec = getargspec(self._attribute(clazz, each))
-            assert iface_spec.args == impl_spec.args
-            assert iface_spec.varargs == impl_spec.varargs
-            assert iface_spec.keywords == impl_spec.keywords
-            if (impl_spec.defaults is None):
-                assert iface_spec.defaults == 0
-            else:
-                assert iface_spec.defaults == len(impl_spec.defaults)
-
+            self._assert_implements(clazz, each)
+    
     def _is_method(self, name):
         try:
             return type(self._attribute(self.interface, name)) == method
         except:
             False
+
+    def _assert_implements(self, clazz, method_name):
+        self._assert_method_presence(clazz, method_name)
+        contract = self._attribute(self.interface, method_name)
+        method_impl = getargspec(self._attribute(clazz, method_name))
+        self._assert_method_arguments(contract, method_impl)
+        self._assert_method_varargs(contract, method_impl)
+        self._assert_method_keyword_args(contract, method_impl)
+        self._assert_method_default_args(contract, method_impl)
+
+    def _assert_method_presence(self, clazz, method_name):
+        assert method_name in dir(clazz)
     
+    def _assert_method_arguments(self, contract, method_impl):
+        assert contract.args == method_impl.args
+        
+    def _assert_method_varargs(self, contract, method_impl):
+        assert contract.varargs == method_impl.varargs
+
+    def _assert_method_keyword_args(self, contract, method_impl):
+        assert contract.keywords == method_impl.keywords
+
+    def _assert_method_default_args(self, contract, method_impl):
+        if (method_impl.defaults is None):
+            assert contract.defaults == 0
+        else:
+            assert contract.defaults == len(method_impl.defaults)
+
     def _attribute(self, clazz, attribute):
         return object.__getattribute__(clazz, attribute)
